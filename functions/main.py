7733f23ea49fb186738dee11be1bd542d0126988
Yan Luiz <yan@nomadbitcoin.xyz>
2025-04-03 21:19:03 -0300

    fix: remove cloud funcion temporally to run locally

--- functions/main.py
+++ functions/main.py
@@ -18,17 +18,17 @@ def run_full_process(req: https_fn.Request) -> None:
             'message': str(e)
         }), 500
 
-@scheduler_fn.on_schedule(schedule="every day 00:00")
-def daily_process_channels(event: scheduler_fn.ScheduledEvent) -> None:
-    """Cloud Function that runs weekly to process all channels."""
-    try:
-        main()
-        return jsonify({
-            'status': 'success', 
-            'message': 'Daily channel processing completed successfully'
-        })
-    except Exception as e:
-        return jsonify({
-            'status': 'error', 
-            'message': str(e)
-        }), 500
+# @scheduler_fn.on_schedule(schedule="every day 00:00")
+# def daily_process_channels(event: scheduler_fn.ScheduledEvent) -> None:
+#     """Cloud Function that runs weekly to process all channels."""
+#     try:
+#         main()
+#         return jsonify({
+#             'status': 'success', 
+#             'message': 'Daily channel processing completed successfully'
+#         })
+#     except Exception as e:
+#         return jsonify({
+#             'status': 'error', 
+#             'message': str(e)
+#         }), 500
